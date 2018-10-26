563bf87fbdcc8186a8a99416b30f887fa7a6f6cb
fender242 <ryan.trigg@gmail.com>
2018-10-26 00:29:12 -0700

    Testing to commit a python file

---
+++ pandy.pd.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(np.random.rand(25).reshape(5, 5))
+
+print(df)
--- pandy.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-
-a = np.random.randint(3,10, (3,3))
-inv = 1/a
-
